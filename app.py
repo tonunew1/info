b0d3026980d8cc7df5d96376e1cea15007c27047
tonunew1 <tonunew1@gmail.com>
2025-12-25 05:16:56 +0600

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 
 def get_account_credentials() -> str:
     # শুধু BD এর UID + password
-    return "uid=4182971809&password=EBE9E2F6E5DBE54430D5CD5F5785B5D92DB016419A45903FEA2B42D8B2B9AB87"
+    return "uid=4309154009&password=93848D86253B465C63271A26D0C5EBA1AFD3121F7C9943243419E27DE2ED81A3"
 
 # === Token Generation ===
 async def get_access_token(account: str):
@@ -197,6 +197,7 @@ if __name__ == '__main__':
 
     # Flask চালু
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
